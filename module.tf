d07e337a4b657e3d8c863fc109509a5aa47a6e6b
Jonathan Stanley <jonathan.stanley@wandb.com>
2024-11-06 14:15:35 -0500

    Syntax fix?

--- module.tf
+++ module.tf
@@ -1,19 +1,19 @@
-# locals {
-#   oidc_envs = {
-#     # "GORILLA_CORS_ORIGINS" = "https://jms-op-lab1.jms.wandb.ml, null"
-#     # "OIDC_ISSUER"      = var.oidc_issuer_url
-#     # "OIDC_CLIENT_ID"   = okta_app_oauth.wandb.client_id
-#     # "OIDC_AUTH_METHOD" = "pkce"
-#     # "ENABLE_REGISTRY_UI" = true
-#     # "GORILLA_CORS_ORIGINS" = "https://jms-op-lab1.jms.wandb.ml, null"
-#     #   "GORILLA_USE_IDENTIFIER_CLAIMS" = true
-#        "GORILLA_CORS_ORIGINS"          = "https://${module.wandb_infra.url}, null"
-#   }
-#   env_vars = merge(
-#     local.oidc_envs,
-#     var.other_wandb_env,
-#   )
-# }
+ locals {
+   oidc_envs = {
+    # "GORILLA_CORS_ORIGINS" = "https://jms-op-lab1.jms.wandb.ml, null"
+    # "OIDC_ISSUER"      = var.oidc_issuer_url
+    # "OIDC_CLIENT_ID"   = okta_app_oauth.wandb.client_id
+    # "OIDC_AUTH_METHOD" = "pkce"
+    "ENABLE_REGISTRY_UI" = true
+    "GORILLA_CORS_ORIGINS" = "https://jms-op-lab1.jms.wandb.ml, null"
+    #   "GORILLA_USE_IDENTIFIER_CLAIMS" = true
+      #  "GORILLA_CORS_ORIGINS"          = "https://${module.wandb_infra.url}, null"
+  }
+  env_vars = merge(
+    local.oidc_envs,
+    var.other_wandb_env,
+  )
+}
 
 module "wandb_infra" {
   source  = "wandb/wandb/aws"
