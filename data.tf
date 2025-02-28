258bc0cc1cb97af602309ad0abccff014180cca4
Jonathan Stanley <jonathan.stanley@wandb.com>
2025-02-28 08:46:23 -0500

    Going to try this again

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aws_eks_cluster" "app_cluster" {
-  name = module.wandb_infra.cluster_id
+  name = module.wandb_infra.cluster_name
 }
 
 data "aws_eks_cluster_auth" "app_cluster" {
-  name = module.wandb_infra.cluster_id
+  name = module.wandb_infra.cluster_name
 }
--- module.tf
+++ module.tf
@@ -1,33 +1,33 @@
-#  locals {
-#    oidc_envs = {
-#     # "GORILLA_CORS_ORIGINS" = "https://jms-op-lab1.jms.wandb.ml, null"
-#     # "OIDC_ISSUER"      = var.oidc_issuer_url
-#     # "OIDC_CLIENT_ID"   = okta_app_oauth.wandb.client_id
-#     # "OIDC_AUTH_METHOD" = "pkce"
-#     "ENABLE_REGISTRY_UI" = true
-#     "GORILLA_CORS_ORIGINS" = "https://jms-op-lab1.jms.wandb.ml, null"
-#     #   "GORILLA_USE_IDENTIFIER_CLAIMS" = true
-#       #  "GORILLA_CORS_ORIGINS"          = "https://${module.wandb_infra.url}, null"
-#   }
-#   env_vars = merge(
-#     local.oidc_envs,
-#     var.other_wandb_env,
-#   )
-# }
+ locals {
+   other_envs = {
+    # "GORILLA_CORS_ORIGINS" = "https://jms-op-lab1.jms.wandb.ml, null"
+    # "OIDC_ISSUER"      = var.oidc_issuer_url
+    # "OIDC_CLIENT_ID"   = okta_app_oauth.wandb.client_id
+    # "OIDC_AUTH_METHOD" = "pkce"
+    "ENABLE_REGISTRY_UI" = true
+    "GORILLA_CORS_ORIGINS" = "https://jms-op-lab1.jms.wandb.ml, null"
+    #   "GORILLA_USE_IDENTIFIER_CLAIMS" = true
+      #  "GORILLA_CORS_ORIGINS"          = "https://${module.wandb_infra.url}, null"
+  }
+  env_vars = merge(
+    local.other_envs,
+    var.other_wandb_env,
+  )
+}
 
 module "wandb_infra" {
   source  = "wandb/wandb/aws"
-  version = "4.22.3"
+  version = "7.9.2"
 
   license              = var.wandb_license
   namespace            = var.namespace
   public_access        = true
   external_dns         = true
-  enable_dummy_dns     = true
-  enable_operator_alb  = true
+  # enable_dummy_dns     = true
+  # enable_operator_alb  = true
   custom_domain_filter = var.domain_name
 
-  other_wandb_env = var.other_wandb_env
+  other_wandb_env = local.env_vars
 
   # private_link_allowed_account_ids = var.private_link_allowed_account_ids
 
@@ -36,25 +36,25 @@ module "wandb_infra" {
   # enable_helm_release    = var.enable_helm_release
 
   deletion_protection            = false
-  database_instance_class        = var.database_instance_class
-  database_engine_version        = var.database_engine_version
-  database_snapshot_identifier   = var.database_snapshot_identifier
-  database_sort_buffer_size      = var.database_sort_buffer_size
+  # database_instance_class        = var.database_instance_class
+  # database_engine_version        = var.database_engine_version
+  # database_snapshot_identifier   = var.database_snapshot_identifier
+  # database_sort_buffer_size      = var.database_sort_buffer_size
   allowed_inbound_cidr           = var.allowed_inbound_cidr
-  allowed_inbound_ipv6_cidr      = ["::/0"]
+  allowed_inbound_ipv6_cidr      = var.allowed_inbound_ipv6_cidr
   eks_cluster_version            = var.eks_cluster_version
   kubernetes_public_access       = true
   kubernetes_public_access_cidrs = ["0.0.0.0/0"]
   domain_name                    = var.domain_name
   zone_id                        = var.zone_id
   subdomain                      = var.subdomain
-  bucket_name                         = var.bucket_name
-  bucket_kms_key_arn                  = var.bucket_kms_key_arn
+  # bucket_name                         = var.bucket_name
+  # bucket_kms_key_arn                  = var.bucket_kms_key_arn
   use_internal_queue                  = true
   size                                = var.size
-  system_reserved_cpu_millicores      = var.system_reserved_cpu_millicores
-  system_reserved_memory_megabytes    = var.system_reserved_memory_megabytes
-  system_reserved_ephemeral_megabytes = var.system_reserved_ephemeral_megabytes
-  system_reserved_pid                 = var.system_reserved_pid
-  aws_loadbalancer_controller_tags    = var.aws_loadbalancer_controller_tags
+  # system_reserved_cpu_millicores      = var.system_reserved_cpu_millicores
+  # system_reserved_memory_megabytes    = var.system_reserved_memory_megabytes
+  # system_reserved_ephemeral_megabytes = var.system_reserved_ephemeral_megabytes
+  # system_reserved_pid                 = var.system_reserved_pid
+  # aws_loadbalancer_controller_tags    = var.aws_loadbalancer_controller_tags
 }
