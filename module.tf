347b3a4f9f3dfe1b283f7f3aa1da3e92d19c1469
jms-wandb <jonathan.stanley@wandb.com>
2025-04-07 10:40:15 -0400

    Update module.tf

--- module.tf
+++ module.tf
@@ -17,7 +17,7 @@
 
 module "wandb_infra" {
   source  = "wandb/wandb/aws"
-  version = "7.9.2"
+  version = "7.12.3"
 
   license              = var.wandb_license
   namespace            = var.namespace
